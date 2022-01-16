a5a4ab1a8c25c6d4903a40fcf12dbcc7840099b5
Matthew Flegg <matthewflegg@outlook.com>
2022-01-16 14:16:29 +0000

    Added Dictionaries to Store Scores + Clear Scores Method

--- ScoringSystemProgram/ScoringSystemWinFormsUI/ScoringProgram.cs
+++ ScoringSystemProgram/ScoringSystemWinFormsUI/ScoringProgram.cs
@@ -12,9 +12,37 @@ namespace ScoringSystemWinFormsUI
 {
     public partial class ScoringProgram : Form
     {
+        #region Constructor
+
         public ScoringProgram()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Declaring Score Storage Dictionaries
+
+        // Creating the eventScores and totalScores dictionaries 
+        private Dictionary<string, int[]> eventScores = new Dictionary<string, int[]>();
+        private Dictionary<string, int> totalScores = new Dictionary<string, int>();
+
+        #endregion
+
+        #region Private Helper Methods
+
+        /// <summary>
+        /// Clears all of the data in the eventScores and totalScores dictionaries by assigning them new, empty dicts
+        /// </summary>
+        /// <param name="eventScores">The dictionary containing the event scores</param>
+        /// <param name="totalScores">The dictionary containing the total scores</param>
+        private void ClearScoreDictionaries(ref Dictionary<string, int[]> eventScores, ref Dictionary<string, int> totalScores)
+        {
+            // Overwrites data in dicts by assigning them empty dicts
+            eventScores = new Dictionary<string, int[]>();
+            totalScores = new Dictionary<string, int>();
+        }
+
+        #endregion
     }
 }
