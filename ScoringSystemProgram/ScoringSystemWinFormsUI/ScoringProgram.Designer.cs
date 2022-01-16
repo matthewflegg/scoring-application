738156a2357b8997a372dd869ead1917f48ccffa
Matthew Flegg <matthewflegg@outlook.com>
2022-01-16 13:21:28 +0000

    Started Building the UI

---
+++ ScoringSystemProgram/ScoringSystemWinFormsUI/ScoringProgram.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace ScoringSystemWinFormsUI
+{
+    partial class ScoringProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.writeToFileButton = new System.Windows.Forms.Button();
+            this.clearDataButton = new System.Windows.Forms.Button();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(12, 41);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(649, 536);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(641, 510);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Enter Contestant Results";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(641, 398);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "View Total Scores";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // writeToFileButton
+            // 
+            this.writeToFileButton.Location = new System.Drawing.Point(12, 12);
+            this.writeToFileButton.Name = "writeToFileButton";
+            this.writeToFileButton.Size = new System.Drawing.Size(75, 23);
+            this.writeToFileButton.TabIndex = 1;
+            this.writeToFileButton.Text = "Write to File";
+            this.writeToFileButton.UseVisualStyleBackColor = true;
+            // 
+            // clearDataButton
+            // 
+            this.clearDataButton.Location = new System.Drawing.Point(93, 12);
+            this.clearDataButton.Name = "clearDataButton";
+            this.clearDataButton.Size = new System.Drawing.Size(75, 23);
+            this.clearDataButton.TabIndex = 2;
+            this.clearDataButton.Text = "Clear Data";
+            this.clearDataButton.UseVisualStyleBackColor = true;
+            // 
+            // ScoringProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 589);
+            this.Controls.Add(this.clearDataButton);
+            this.Controls.Add(this.writeToFileButton);
+            this.Controls.Add(this.tabControl);
+            this.Name = "ScoringProgram";
+            this.Text = "Scoring System Program";
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button writeToFileButton;
+        private System.Windows.Forms.Button clearDataButton;
+    }
+}
+
